2374b3b6a23e02fff637a40fe29a7062dfd4bf8a
Harm Ellis <harmellis@outlook.com>
2025-07-22 21:01:47 +0200

    Remove the SmartRC-CC1101-Driver-Lib from add_library.

--- esphome/components/cc1101/__init__.py
+++ esphome/components/cc1101/__init__.py
@@ -41,7 +41,6 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     cg.add_library("SPI", None)
     cg.add_library("EEPROM", None)
-    cg.add_library("SmartRC-CC1101-Driver-Lib", None)
     await cg.register_component(var, config)
 
     clk_pin = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
